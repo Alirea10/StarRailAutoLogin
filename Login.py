37c2a9b74a21f723ce62d14eab3f0c1aedca648a
Alirea <2981883527@qq.com>
2025-08-16 21:15:31 +0800

    feat(bilibili)：适配了bilibili的登录

--- Login.py
+++ Login.py
@@ -11,12 +11,14 @@ from SRACore.utils.WindowsProcess import find_window, Popen
 from SRACore.utils.Logger import logger
 from SRACore.utils.SRAOperator import SRAOperator
 
+
 # 工具函数
 def is_admin():
     try:
         return ctypes.windll.shell32.IsUserAnAdmin()
     except Exception:
         return False
+
 
 def run_as_admin():
     if not is_admin():
@@ -26,23 +28,31 @@ def run_as_admin():
             None, "runas", sys.executable, params, None, 1)
         sys.exit()
 
+
 def check_any(img_list: list, interval=0.5, max_time=40):
     return SRAOperator.checkAny(img_list, interval, max_time)
+
 
 def click(*args, **kwargs) -> bool:
     return SRAOperator.click_img(*args, **kwargs)
 
+
 def click_point(x: int = None, y: int = None) -> bool:
     return SRAOperator.click_point(x, y)
+
 
 def press_key(key: str, presses: int = 1, interval: float = 2, wait: float = 0) -> bool:
     return SRAOperator.press_key(key, presses, interval, wait)
 
+
 def check(img_path, interval=0.5, max_time=40):
     return SRAOperator.check(img_path, interval, max_time)
 
+
 def write(content: str = "") -> bool:
     return SRAOperator.write(content)
+
+
 # 检查Starward是否安装并注册了URL协议
 def check_starward_URL(scheme) -> bool:
     try:
@@ -64,6 +74,9 @@ class Login:
 
     @staticmethod
     def path_check(path, path_type="StarRail"):
+        if path_type == "Starward":
+            logger.debug("使用 Starward 启动，无需路径校验")
+            return True
         if path:
             if path.replace('\\', '/').split('/')[-1].split('.')[0] != path_type:
                 logger.warning("你尝试输入一个其他应用的路径")
@@ -144,8 +157,8 @@ class Login:
                 return False
 
         # 打开游戏后进行登录
-        if channel == 0:
-            if login_flag:
+        if login_flag:
+            if channel == 0:
                 match self.login_official(account, password):
                     case 0:
                         logger.warning("登录失败")
@@ -158,26 +171,26 @@ class Login:
                     case _:
                         logger.error("未知登录状态")
                         return False
+            elif channel == 1:
+                self.login_bilibili(account, password)
+                if check("res/img/quit.png"):
+                    self.start_game_click()
+                else:
+                    logger.warning("加载时间过长，请重试")
+                    return False
             time.sleep(3)
-            result = check_any(["res/img/quit.png","res/img/enter_game.png"], interval=0.5, max_time=30)
-            if result==0:
+            result = check_any(["res/img/quit.png", "res/img/enter_game.png"], interval=0.5, max_time=30)
+            if result == 0:
                 self.start_game_click()
-            elif result==1:
+            elif result == 1:
                 click("res/img/enter_game.png")
                 self.start_game_click()
-        elif channel == 1:
-            self.login_bilibili(account, password)
-            if check("res/img/quit.png"):
-                self.start_game_click()
-            else:
-                logger.warning("加载时间过长，请重试")
-                return False
+
         return self.wait_game_load()
 
     @staticmethod
     def login_official(account, password):
         logger.info("官服登录中")
-        time.sleep(3)
         result = check_any(
             # 分别对应登录状态 0: 未登录, 1: 已有登录, 2: 已有登录, 3: 已进入游戏
             ["res/img/login_page.png", "res/img/welcome.png", "res/img/quit.png", "res/img/chat_enter.png"])
@@ -188,11 +201,11 @@ class Login:
             logger.warning("检测到游戏分辨率不为16:9, 自动登录可能无法按预期运行")
 
         if result is not None and result == 3:
-            return result
+            return 3
         # 若已经登录，则退出登录
         elif result == 1 or result == 2:
             # 等火车头...
-            if check("res/img/logout.png",interval=0.5, max_time=30):
+            if check("res/img/logout.png", interval=0.5, max_time=30):
                 click("res/img/logout.png")
             time.sleep(0.1)
             click("res/img/quit2.png")
@@ -201,7 +214,7 @@ class Login:
         else:
             click("res/img/login_other.png")
         if not click("res/img/login_with_account.png"):
-            logger.error("发生错误，错误编号10")
+            logger.error("发生错误，无法点击登录其他账号按钮，可能有未适配的界面")
             return 0
         logger.info("登录到" + account)
         time.sleep(1)
@@ -214,7 +227,7 @@ class Login:
         SRAOperator.paste()
         click("res/img/agree.png", -158)
         if not click("res/img/enter_game.png"):
-            logger.error("发生错误，错误编号9")
+            logger.error("发生错误，找不到登录按钮，请检查页面是否有更新或用户协议是否已同意")
             return 0
         if check("res/img/welcome.png", interval=0.2, max_time=20):
             logger.info("登录成功")
@@ -226,10 +239,10 @@ class Login:
     @staticmethod
     def login_bilibili(account, password):
         logger.info("B服登录中")
-        time.sleep(3)
         result = check_any(
             # 分别对应登录状态 0: 未登录, 1: 已有登录, 2: 已有登录, 3: 已进入游戏
-            ["res/img/bilibili_login_page.png", "res/img/bilibili_welcome.png", "res/img/quit.png", "res/img/chat_enter.png"])
+            ["res/img/bilibili_login_page.png", "res/img/bilibili_welcome.png", "res/img/quit.png",
+             "res/img/chat_enter.png"])
 
         logger.info(f"登录状态 {result}")
         resolution = SRAOperator.resolution_detect()
@@ -241,7 +254,7 @@ class Login:
         # 若已经登录，则退出登录
         elif result == 1 or result == 2:
             # 等火车头...
-            if check("res/img/logout.png",interval=0.5, max_time=30):
+            if check("res/img/logout.png", interval=0.5, max_time=30):
                 click("res/img/logout.png")
             time.sleep(0.1)
             click("res/img/enter.png")
@@ -252,7 +265,9 @@ class Login:
         if not click("res/img/bilibili_login_with_account.png"):
             logger.error("发生错误，错误编号10")
             return 0
-        logger.info("登录到" + account)
+        logger.info("尝试登录到" + account[:3] + "****" + account[-2:])
+        if check("res/img/bilibili_account.png", interval=0.2, max_time=20):
+            click("res/img/bilibili_account.png")
         time.sleep(1)
         SRAOperator.copy(account)
         SRAOperator.paste()
@@ -261,31 +276,22 @@ class Login:
         time.sleep(0.2)
         SRAOperator.copy(password)
         SRAOperator.paste()
-        click("res/img/bilibili_agree.png", -260)
-        if not click("res/img/enter_game.png"):
-            logger.error("发生错误，错误编号9")
-            return 0
+        if check("res/img/bilibili_agree2.png"):
+            click("res/img/bilibili_agree2.png", -26)
+        click("res/img/bilibili_enter_game.png")
         if check("res/img/welcome.png", interval=0.2, max_time=20):
             logger.info("登录成功")
             return 1
-        else:
-            logger.warning("长时间未成功登录，可能密码错误或需要新设备验证")
+        elif check("res/img/bilibili_verify.png"):
+            logger.warning("遇到b站验证码！请手动完成！")
+            for wait_time in range(10):
+                logger.warning(f"请手动完成b站验证码!剩余{10 - wait_time}秒")
+                time.sleep(1)
+                if check("res/img/welcome.png", interval=0.2, max_time=1):
+                    logger.info("登录成功")
+                    return 1
+            logger.error("验证码超时，登录失败")
             return 0
-
-        # if not check("res/img/bilibili_login.png", interval=0.2, max_time=20):
-        #     logger.error("检测超时，编号3")
-        #     return False
-        # if click('res/img/bilibili_account.png'):
-        #     logger.info("登录到" + account)
-        #     time.sleep(0.5)
-        #     write(account)
-        # if click('res/img/bilibili_pwd.png'):
-        #     time.sleep(0.5)
-        #     write(password)
-        # click('res/img/bilibili_remember.png')
-        # click("res/img/bilibili_read.png", x_add=-30)
-        # click("res/img/bilibili_login.png")
-        # return True
 
     @staticmethod
     def start_game_click():
@@ -316,7 +322,6 @@ class Login:
             time.sleep(1)
 
 
-
 # 主程序入口
 if __name__ == "__main__":
     logger.info("程序开始")
@@ -332,7 +337,7 @@ if __name__ == "__main__":
     config_id = sys.argv[1] if len(sys.argv) > 1 else "1"
     # 如果没有给定参数，默认使用第一个配置
     config_path = os.path.join(os.path.dirname(__file__), "config/config.json")
-    logger.info(f"加载配置文件: {config_path}，使用第 {config_id} 配置")
+    logger.debug(f"加载配置文件: {config_path}，使用第 {config_id} 配置")
     with open(config_path, "r", encoding="utf-8") as f:
         all_config = json.load(f)
 
@@ -348,12 +353,13 @@ if __name__ == "__main__":
         server=config.get("server"),
         username=config.get("username"),
         password=config.get("password")
-)
+    )
 
     # 串行任务队列
     tasks = [
         (login.path_check, (login.game_path, login.game_type)),
-        (login.start_game, (login.game_path, login.game_type, int(login.server), login.enable_login, login.username, login.password)),
+        (login.start_game,
+         (login.game_path, login.game_type, int(login.server), login.enable_login, login.username, login.password)),
         (login.check_game, ()),
     ]
     for func, args in tasks:
