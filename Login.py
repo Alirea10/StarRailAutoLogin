afcbc3874bf4d7f6d1c2bea1bb4a3c16706e9483
Alirea <2981883527@qq.com>
2025-10-04 09:13:00 +0800

    fix(login): 修复主动登录时的一处点击问题

--- Login.py
+++ Login.py
@@ -784,6 +784,16 @@ class Login:
                 return False
 
             logger.debug("[_input_credentials] 登录凭据输入完成")
+
+            # 获取屏幕中心点坐标
+            x, y = SRAOperator.get_screen_center()
+
+            # 检查是否处于登录页(以12+标志为准)，进入火车头界面
+            if check("res/img/12+.png",0.5,10):
+                click_point(x, y)
+                time.sleep(3)
+
+            logger.info("[_input_credentials] 点击开始游戏，即将进入火车头")
             return True
 
     def _wait_for_login_result(self, channel):
